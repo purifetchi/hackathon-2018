15ac1fe3cc28281a62bfdfb52431e9bc739ad671
RazorMeister <battnik90@gmail.com>
2018-04-18 13:46:05 +0200

    Error 500

--- app.py
+++ app.py
@@ -82,7 +82,14 @@ def route_logout():
 @app.errorhandler(404)
 def page_not_found(e):	
 	return render_template("404.html"), 404
+
+# 505
+@app.errorhandler(500)
+def page_not_found(e):	
+	return render_template("500.html"), 500
 	
+	
+
 
 app.secret_key = "dupa"
     
