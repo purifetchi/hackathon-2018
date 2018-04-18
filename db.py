8983b3f11aaee4676237732de21c9d75007a7dc4
KajetanLarwa <38082013+KajetanLarwa@users.noreply.github.com>
2018-04-18 12:40:30 +0200

    db class

--- db.py
+++ db.py
@@ -3,3 +3,19 @@ import sqlite3, time, json
 class database():
     def __init__(self):
         self.db = sqlite3.connect("./database.db")
+    def is_user(self, username):
+        cursor = self.db.cursor()
+        cursor.execute("SELECT * FROM `users` WHERE `username`=?", (username,))
+        row = cursor.fetchone()
+
+        if row is None:
+            return False
+        else: 
+            return True
+    def get_user_info(self, username):
+        cursor = self.db.cursor()
+        cursor.execute("SELECT * FROM `users` WHERE `username`=?", (username,))
+    def register_user(self, username, password):
+        pass
+    def get_user_posts(self, username):
+        pass
