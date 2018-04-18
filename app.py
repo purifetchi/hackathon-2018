144b0385144cf4335c413078aa5004f5cf19bb34
RazorMeister <battnik90@gmail.com>
2018-04-18 11:37:09 +0200

    Update v2

--- app.py
+++ app.py
@@ -8,34 +8,39 @@ app = Flask(__name__)
 def route_index():
 	return "Test"
 	
+# Logowanie
+@app.route("/login")
+def route_login():
+	return "Logowanie"
+
 # Rejestracja
 @app.route("/register")
-def route_register()
+def route_register():
 	return "Zarejestruj sie juz teraz! Polecam XBot"
 	
 # Profil uzytkownika
 @app.route("/profile/<username>")
-def route_profile(username)
+def route_profile(username):
 	return "Witaj" + username
 
 # Ustawienia profilu
 @app.route("/profile/settings")
-def route_settings()
+def route_settings():
 	return "Ustawienia"
 	
 # Follow
 @app.route("/profile/<username>/follow")
-def route_followe(username)
+def route_followe(username):
 	return "Wlasnie followujesz uzytkowika: " + username
 	
 # Cofanie followa
 @app.route("/profile/<username>/unfollow")
-def route_unfollow(username)
+def route_unfollow(username):
 	return "Juz nie followujesz uzytkowika: " + username
 	
 # Wylogowywanie
 @app.route("/logout")
-def route_logout()
+def route_logout():
 	return "Wylogowano!"
 	
 
