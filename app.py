27eafb6e94921ccc67eaefc7821f1c11bc92068f
RazorMeister <battnik90@gmail.com>
2018-04-18 12:09:59 +0200

    404

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def route_logout():
 	# 404
 	@app.errorhandler(404)
 	def page_not_found(e):	
-		return render_template("404.html"), 404
+		return render_template("templates/404.html"), 404
 	
 
 app.secret_key = "dupa"
