e168f7335c6f8ac416ab54ec1e8a8af92447c78c
RazorMeister <battnik90@gmail.com>
2018-04-18 11:23:13 +0200

    profil

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
+// Strona glowna
 @app.route("/")
 def route_index():
 	return "Test"
+	
+// Profil uzytkownika
+@app_route("/profile/<username>")
+def route_profile(username)
+	return "Witaj" + username
 
 app.secret_key = "dupa"
     
