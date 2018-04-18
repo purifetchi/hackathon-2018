752859b8d24129b0d8e0479e502bb6b674526174
KajetanLarwa <38082013+KajetanLarwa@users.noreply.github.com>
2018-04-18 11:19:15 +0200

    secret_key

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
 @app.route("/")
 def route_index():
 	return "Test"
+
+app.secret_key = "dupa"
     
